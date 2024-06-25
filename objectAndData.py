65031dd7c4eb5a5383852b68253e53fc2bcb6386
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-06-25 16:32:01 +0530

    datatypes

---
+++ objectAndData.py
@@ -0,0 +1,26 @@
+x=5
+print(type(x))#this will show the type of veriable x which is <class 'int'>
+
+#<class 'list'>
+y=["Bhavesh","Shruti","Abhi","Sai"]
+print(type(y))
+
+#<class 'tuple'>
+z=("Bhavesh","Shruti","Abhi","Sai")
+print(type(z))
+
+
+#<class 'set'>
+a={'a','b','c','d','d'}
+print(a)#dont allow repeated elements 
+print(type(a))
+
+#<class 'dict'>
+b={'name':'Bhavesh','age':22}
+print(b)
+print(type(b))
+
+#frozenset
+c=frozenset({'a','b','c','d','d'})
+print(c)
+print(type(c))
