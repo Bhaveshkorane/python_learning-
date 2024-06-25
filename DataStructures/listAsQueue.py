cda319b64ab94d6c72933bcce7371296f3370ca2
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-06-25 18:02:03 +0530

    listasQeueue

---
+++ DataStructures/listAsQueue.py
@@ -0,0 +1,30 @@
+#list as Queue
+
+Queue=[1,23,65,233]
+
+def enqueue(num):
+    Queue.append(num)
+
+def deque():
+    if(len(Queue)==0):
+        print('The list is empty')
+    else:
+        Queue.pop(0);
+
+enqueue(100)
+enqueue(500)
+
+
+print("After adding elements in the queue:",Queue)
+deque()
+deque()
+
+
+print('After removing the element at the start:',Queue)
+deque()
+deque()
+deque()
+deque()
+deque()
+deque()
+
