07b5381411e6ae245e09fdeb40011fdeb07e4a32
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-07-01 14:45:33 +0530

    dictionary comprehension

--- pytonComprehension/dictionaryComprehension.py
+++ pytonComprehension/dictionaryComprehension.py
@@ -1,1 +1,6 @@
 #listComprehension
+#zip function is used to combine two lists or itrables 
+roll=[1,2,3,4,5]
+name=['bk','sp','am','sk','sg']
+myDict={roll:name for roll,name in zip(roll,name) }
+print("This is my dictionary:",myDict)
