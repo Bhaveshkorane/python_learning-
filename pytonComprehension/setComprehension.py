86515bb4d672ca9c1ec78f5fde859d9993f63f48
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-07-01 14:38:19 +0530

    Set Comprehension

---
+++ pytonComprehension/setComprehension.py
@@ -0,0 +1,9 @@
+#setComprehension
+ls=[1,2,56,45,78,65,255,856,369,1,2,56,87]
+
+mySet={a for a in ls}
+print('This is my set:',mySet)
+
+
+myNewSet={a for a in range(1,11)}
+print("this is myNewSet:",myNewSet)
